504bedb00061904a042f321849008ab855ef39eb
Ankita7520 <ankitaacharjya1999@gmail.com>
2023-06-26 11:29:33 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Navbar from "./Navbar";
+import Flask from "./Flask";
 
 
 const App=()=>{
     return(
     <div>
-        <BrowserRouter>
+        {/* <BrowserRouter>
         <Routes>
             <Route path="/" element={<Navbar></Navbar>}></Route>
         </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
+        <Flask></Flask>
     </div>
     )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import "./Comp.css";
 import {SlMagnifier,SlLocationPin} from "react-icons/sl";
 import img1 from "./images/one.jpg";
@@ -9,7 +9,10 @@ import img5 from "./images/five.jpg";
 import img6 from "./images/10.png";
 import img7 from "./images/12.png";
 import img8 from "./images/13.png";
-import img14 from "./images/14.jpg"
+import img14 from "./images/14.jpg";
+import {CiFacebook,CiTwitter} from "react-icons/ci";
+import {AiOutlineLinkedin,AiOutlineYoutube,AiOutlineInstagram} from "react-icons/ai"
+
 
 
 
@@ -115,19 +118,229 @@ const Navbar=()=>{
                     <img src={img7} alt="" className="trust-img"/>
                 </div>
             </div>
-            <div>
+            
+            
+        </div>
+        <div className="featured">
                 <h3>Featured <span>Jobs</span> for you</h3>
             </div>
             <div>
-                <div>
-                    <img src={img14} alt="" />
-                </div>
-                <div>
-                    <button></button>
-                </div>
+        <div className="boxx-main">
+        <div className="boxx-1">
+           <div className="boxx-2">
+           <img src={img1} alt="" className="box-img"/>
+            <p className="box-head">Microsoft BizApps_F&O_Technical_Senior</p>
+            <p className="box-time">Full Time 3Y-6Y</p>
+            <div style={{display:"flex"}}>
+            <SlLocationPin className="box-icon"></SlLocationPin>
+              <p className="box-loc">India</p>
             </div>
+           </div>
+           <div style={{display:"flex"}}>
+            <p className="box-bott">EY GODS</p>
+            <div className="box-bottt">
+            <p className="box-bot">1week ago</p>
+            </div>
+           </div>
         </div>
-     </div>
+        <div className="boxx-1">
+           <div className="boxx-2">
+           <img src={img1} alt="" className="box-img"/>
+            <p className="box-head">Microsoft BizApps_F&O_Technical_Senior</p>
+            <p className="box-time">Full Time 3Y-6Y</p>
+            <div style={{display:"flex"}}>
+            <SlLocationPin className="box-icon"></SlLocationPin>
+              <p className="box-loc">India</p>
+            </div>
+           </div>
+           <div style={{display:"flex"}}>
+            <p className="box-bott">EY GODS</p>
+            <div className="box-bottt">
+            <p className="box-bot">1week ago</p>
+            </div>
+           </div>
+        </div>
+        <div className="boxx-1">
+           <div className="boxx-2">
+           <img src={img1} alt="" className="box-img"/>
+            <p className="box-head">Microsoft BizApps_F&O_Technical_Senior</p>
+            <p className="box-time">Full Time 3Y-6Y</p>
+            <div style={{display:"flex"}}>
+            <SlLocationPin className="box-icon"></SlLocationPin>
+              <p className="box-loc">India</p>
+            </div>
+           </div>
+           <div style={{display:"flex"}}>
+            <p className="box-bott">EY GODS</p>
+            <div className="box-bottt">
+            <p className="box-bot">1week ago</p>
+            </div>
+           </div>
+        </div>
+        <div className="boxx-1">
+           <div className="boxx-2">
+           <img src={img1} alt="" className="box-img"/>
+            <p className="box-head">Microsoft BizApps_F&O_Technical_Senior</p>
+            <p className="box-time">Full Time 3Y-6Y</p>
+            <div style={{display:"flex"}}>
+            <SlLocationPin className="box-icon"></SlLocationPin>
+              <p className="box-loc">India</p>
+            </div>
+           </div>
+           <div style={{display:"flex"}}>
+            <p className="box-bott">EY GODS</p>
+            <div className="box-bottt">
+            <p className="box-bot">1week ago</p>
+            </div>
+           </div>
+        </div>
+        <div className="boxx-1">
+           <div className="boxx-2">
+           <img src={img1} alt="" className="box-img"/>
+            <p className="box-head">Microsoft BizApps_F&O_Technical_Senior</p>
+            <p className="box-time">Full Time 3Y-6Y</p>
+            <div style={{display:"flex"}}>
+            <SlLocationPin className="box-icon"></SlLocationPin>
+              <p className="box-loc">India</p>
+            </div>
+           </div>
+           <div style={{display:"flex"}}>
+            <p className="box-bott">EY GODS</p>
+            <div className="box-bottt">
+            <p className="box-bot">1week ago</p>
+            </div>
+           </div>
+        </div>
+        <div className="boxx-1">
+           <div className="boxx-2">
+           <img src={img1} alt="" className="box-img"/>
+            <p className="box-head">Microsoft BizApps_F&O_Technical_Senior</p>
+            <p className="box-time">Full Time 3Y-6Y</p>
+            <div style={{display:"flex"}}>
+            <SlLocationPin className="box-icon"></SlLocationPin>
+              <p className="box-loc">India</p>
+            </div>
+           </div>
+           <div style={{display:"flex"}}>
+            <p className="box-bott">EY GODS</p>
+            <div className="box-bottt">
+            <p className="box-bot">1week ago</p>
+            </div>
+           </div>
+        </div>
+      </div>
+      </div>
+      <div>
+        <div style={{textAlign:"center"}}>
+        <h3>Featured <span>Gigs </span>for you</h3>
+        </div>
+        <div className="gigs-main">
+           <div className="gigs-box">
+            <div className="gigs-body">
+                <img src={img4} alt="" className="gigs-img"/>
+                <p className="gigs-head">Content Creator</p>
+                 <p className="gigs-hour">Hourly</p>
+                 <div className="gigs-location">
+                    <SlLocationPin className="gigs-loc"></SlLocationPin>
+                    <p className="gigs-city">Bangalore</p>
+                 </div>
+            </div>
+            <div className="gigs-foot">
+                <p className="gigs-footer">NexionPro Services...</p>
+            </div>
+           </div>
+           <div className="gigs-box">
+            <div className="gigs-body">
+                <img src={img4} alt="" className="gigs-img"/>
+                <p className="gigs-head">Content Creator</p>
+                 <p className="gigs-hour">Hourly</p>
+                 <div className="gigs-location">
+                    <SlLocationPin className="gigs-loc"></SlLocationPin>
+                    <p className="gigs-city">Bangalore</p>
+                 </div>
+            </div>
+            <div className="gigs-foot">
+                <p className="gigs-footer">NexionPro Services...</p>
+            </div>
+           </div>
+           <div className="gigs-box">
+            <div className="gigs-body">
+                <img src={img4} alt="" className="gigs-img"/>
+                <p className="gigs-head">Content Creator</p>
+                 <p className="gigs-hour">Hourly</p>
+                 <div className="gigs-location">
+                    <SlLocationPin className="gigs-loc"></SlLocationPin>
+                    <p className="gigs-city">Bangalore</p>
+                 </div>
+            </div>
+            <div className="gigs-foot">
+                <p className="gigs-footer">NexionPro Services...</p>
+            </div>
+           </div>
+           <div className="gigs-box">
+            <div className="gigs-body">
+                <img src={img4} alt="" className="gigs-img"/>
+                <p className="gigs-head">Content Creator</p>
+                 <p className="gigs-hour">Hourly</p>
+                 <div className="gigs-location">
+                    <SlLocationPin className="gigs-loc"></SlLocationPin>
+                    <p className="gigs-city">Bangalore</p>
+                 </div>
+            </div>
+            <div className="gigs-foot">
+                <p className="gigs-footer">NexionPro Services...</p>
+            </div>
+           </div>
+           <div className="gigs-box">
+            <div className="gigs-body">
+                <img src={img4} alt="" className="gigs-img"/>
+                <p className="gigs-head">Content Creator</p>
+                 <p className="gigs-hour">Hourly</p>
+                 <div className="gigs-location">
+                    <SlLocationPin className="gigs-loc"></SlLocationPin>
+                    <p className="gigs-city">Bangalore</p>
+                 </div>
+            </div>
+            <div className="gigs-foot">
+                <p className="gigs-footer">NexionPro Services...</p>
+            </div>
+           </div>
+           <div className="gigs-box">
+            <div className="gigs-body">
+                <img src={img4} alt="" className="gigs-img"/>
+                <p className="gigs-head">Content Creator</p>
+                 <p className="gigs-hour">Hourly</p>
+                 <div className="gigs-location">
+                    <SlLocationPin className="gigs-loc"></SlLocationPin>
+                    <p className="gigs-city">Bangalore</p>
+                 </div>
+            </div>
+            <div className="gigs-foot">
+                <p className="gigs-footer">NexionPro Services...</p>
+            </div>
+           </div>
+        </div>
+      </div>
+      <div className="foot-main">
+                <footer>
+                   <div className="foot-first">
+                   <a href="" className="foot-1">Home</a>
+                        <a href="" className="foot-1">About Us</a>
+                        <a href="" className="foot-1">Contact Us</a>
+                        <a href="" className="foot-1">Giveaway</a>
+                        <a href="" className="foot-1">Blog</a>
+                        <a href="" className="foot-1">TermsofUse&PrivacyPolicy</a>
+                   </div>
+                    <div style={{display:"flex"}}>
+                        <p className="foot-2">All rights reserved@2023FindProTechnologySolutionsPvt.Ltd</p>
+                        <p className="foot-3">StayConnected<CiFacebook className="foot-4"></CiFacebook><AiOutlineLinkedin className="foot-4"></AiOutlineLinkedin><CiTwitter className="foot-4"></CiTwitter><AiOutlineInstagram className="foot-4"></AiOutlineInstagram><AiOutlineYoutube className="foot-4"></AiOutlineYoutube></p>
+                    </div>
+                </footer>
+
+            </div>
+            </div>
+
+     
      )
     
 }
